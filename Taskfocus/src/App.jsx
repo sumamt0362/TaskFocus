488e1cd9c5372f6d0142c24f5fdc1a7978e58f9b
suma M T <mtsuma03@gmail.com>
2025-10-17 21:32:39 +0530

    second commit

--- Taskfocus/src/App.jsx
+++ Taskfocus/src/App.jsx
@@ -1,7 +1,9 @@
+
 import Taskform from "./Components/Taskform";
 import TaskList from "./Components/TaskList";
 import Progresstracker from "./Components/Progresstracker";
 import { useEffect, useState } from "react";
+
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
@@ -15,14 +17,27 @@ export default function App() {
     setTasks([...tasks,task]);
   }
 
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+     newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+
+  const deleteTask = (index) => {
+      setTasks(tasks.filter((_, i) => i != index));
+  }
+
   return(
     <div>
       <h1>Task Focus</h1>
       <p>Our friendly TaskManager</p>
       <Taskform addTask = {addTask}/>
-      <TaskList />
-      <Progresstracker />
+      <TaskList tasks = {tasks} 
+      updateTask = {updateTask}
+      deleteTask = {deleteTask}/>
+      <Progresstracker tasks = {tasks}/>
       <button>Clear all tasks</button>
     </div>
   )
 }
+
--- Taskfocus/src/Components/Progresstracker.jsx
+++ Taskfocus/src/Components/Progresstracker.jsx
@@ -1,7 +1,19 @@
-export default function Progresstracker() {
+export default function Progresstracker({tasks}) {
+  
+  const completedTasks = tasks.filter((t)=>t.completed).length;
+  const totaltasks = tasks.length;
+  const percenteage = totaltasks == 0 ? 0: (completedTasks/totaltasks) * 100;
   return (
-    <div>
-      <h1>Progress Tracker Component</h1>
+    <div className="progress-tracker">
+      <p>
+        {completedTasks} of {totaltasks} tasks completed
+      </p>
+      <div className="progress-bar">
+        <div
+          className="progress"
+          style={{ width: `${percenteage}%` }}
+        ></div>
+      </div>
     </div>
-  )
+  );
 }
